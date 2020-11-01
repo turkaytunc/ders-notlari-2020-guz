0aa54f261031a109a115a1383c3cbce44d1b5487
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-01 18:09:49 +0300

    update client side routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './app.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { NavigationBar } from './components/navigation-bar/NavigationBar';
+import { Footer } from './components/footer/Footer';
+import { WelcomePage } from './components/welcome-page/WelcomePage';
+import { FormelDiller } from './components/formel-diller/FormelDiller';
 
 function App() {
   return (
@@ -8,6 +11,11 @@ function App() {
       <div className="app">
         <div className="app-title">Ders Notlari</div>
         <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+          <Route path="/formel-diller" component={FormelDiller} />
+        </Switch>
+        <Footer />
       </div>
     </Router>
   );
