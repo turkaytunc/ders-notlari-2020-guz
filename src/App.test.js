a17d16eb45e831dfc752057b700c7ad93f5e7a9a
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-01 17:18:09 +0300

    update packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Ders Notlari link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ders Notlari/i);
   expect(linkElement).toBeInTheDocument();
 });
