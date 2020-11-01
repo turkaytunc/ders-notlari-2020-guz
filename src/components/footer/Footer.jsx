97501524d3b7725a39d15164ba236881032df32e
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-01 20:05:45 +0300

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,5 +3,39 @@ import './footer.scss';
 import React from 'react';
 
 export const Footer = () => {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-social">
+        <div className="footer-social-container">
+          <a
+            href="https://github.com/turkaytunc"
+            className="footer-link"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              src={`${process.env.PUBLIC_URL}/footer-icons/github-icon-white.png`}
+              alt="github"
+            />
+            Github
+          </a>
+        </div>
+        <div className="footer-social-container">
+          <a
+            href="https://www.linkedin.com/in/turkaytunc/"
+            className="footer-link"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              src={`${process.env.PUBLIC_URL}/footer-icons/linkedin.png`}
+              alt="linkedin"
+            />
+            LinkedIn
+          </a>
+        </div>
+      </div>
+      <div className="footer-copy">&copy; 2020 Türkay Tunç</div>
+    </div>
+  );
 };
