a08260f254030af4b64778598bb026b5adb38885
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-01 19:16:07 +0300

    update fonts

--- src/components/mobil-uygulama/MobilUygulama.jsx
+++ src/components/mobil-uygulama/MobilUygulama.jsx
@@ -3,5 +3,5 @@ import './mobil-uygulama.scss';
 import React from 'react';
 
 export const MobilUygulama = () => {
-  return <div></div>;
+  return <div className="mobil-uygulama-container"></div>;
 };
