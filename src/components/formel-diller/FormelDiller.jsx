53352d0a3f142c46f5d01ed521b4a603b081f209
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-09 22:02:00 +0300

    add first week

--- src/components/formel-diller/FormelDiller.jsx
+++ src/components/formel-diller/FormelDiller.jsx
@@ -21,6 +21,18 @@ export const FormelDiller = () => {
           <a href="#otomatlar" className="index-link">
             Otomatlar
           </a>
+          <a href="#turing-makinasi" className="index-link">
+            Turing Makinası
+          </a>
+          <a href="#chomsky" className="index-link">
+            Chomsky Hiyerarşisi
+          </a>
+          <a href="#formel-dillerin-betimlenmesi" className="index-link">
+            Formel Di̇lleri̇n Beti̇mlenmesi̇
+          </a>
+          <a href="#formel-diller-tanimlar" className="index-link">
+            Formel Diller Tanımlar
+          </a>
         </section>
         <section className="notes">
           <div id="notlar">
@@ -177,11 +189,110 @@ export const FormelDiller = () => {
               - 1950'li yıllarda Stephen Kleene, sonlu bir bellekle donatılmış
               soyut durum makineleri olan sonlu otomatları ortaya atmıştır.
             </p>
+            <p
+              id="turing-makinasi"
+              style={{ fontSize: '1.5em', marginBottom: '1em' }}
+            >
+              Turing Makinası
+            </p>
             <p className="notes-paragraph">
               - Alan Turing 1936 yılında Turing Makinası kavramını ortaya
               atmıştır.
             </p>
-            <p className="notes-paragraph"></p>
+            <p className="notes-paragraph">
+              - Turing makineleri, herhangi bir algoritmanın işleyişinin
+              benzetimini yapabileceği gibi daha yüksek seviyelerdeki biçimsel
+              dilleri de tanıyabilmektedir.
+            </p>
+            <p id="chomsky" style={{ fontSize: '1.5em', marginBottom: '1em' }}>
+              Chomsky Hiyerarşisi
+            </p>
+            <p className="notes-paragraph">
+              - Chomsky hiyerarşisi ile farklı tipteki otomatlar arasında bir
+              eşleniklik vardır. Bu nedenle formel dillere ilişkin kuramlar hem
+              gramerler hem de otomatlar olarak ele alınabilmektedir.
+            </p>
+            <p className="notes-paragraph">
+              <strong>Karmaşıklık seviyesi-0</strong>
+            </p>
+            <p className="notes-paragraph">
+              - Özyinelemeli - Sayılabilir Diller : Turing Makinesi (Recursively
+              – Enumerable Languages : Turing Machine)
+            </p>
+            <p className="notes-paragraph">
+              <strong>Karmaşıklık seviyesi-1</strong>
+            </p>
+            <p className="notes-paragraph">
+              - Bağlama - Duyarlı Diller : Doğrusal – Sınırlandırılmış
+              otomatalar (Context – Sensitive Languages : Linear – Bounded
+              Automata)
+            </p>
+            <p className="notes-paragraph">
+              <strong>Karmaşıklık seviyesi-2</strong>
+            </p>
+            <p className="notes-paragraph">
+              - Bağlam - Bağımsız Diller : Bas – Bırak otomataları (Context –
+              Free Languages : Push – Down Automata)
+            </p>
+            <p className="notes-paragraph">
+              <strong>Karmaşıklık seviyesi-3</strong>
+            </p>
+            <p className="notes-paragraph">
+              - Düzenli Diller : Sonlu - Durum otomataları (Regular Language :
+              Finite – State Automata)
+            </p>
+            <p
+              id="formel-dillerin-betimlenmesi"
+              style={{ fontSize: '1.5em', marginBottom: '1em' }}
+            >
+              Formel Di̇lleri̇n Beti̇mlenmesi̇
+            </p>
+            <p className="notes-paragraph">
+              <strong>Üretimsel (Generative) Yaklaşım</strong>
+            </p>
+            <p className="notes-paragraph">
+              1- Bir başlangıç sembolü ile başlama,
+              <br />
+              2- Yeniden yazma (rewrite) kuralları ile genişletme, <br />
+              3- Dile ait bir ifade üretildiğinde de durma şeklinde
+              gerçekleştirilmektedir.
+            </p>
+            <p className="notes-paragraph">
+              <strong>Tanıma (Recognition) Yaklaşım</strong>
+            </p>
+            <p className="notes-paragraph">
+              1- Bir başlangıç durumu ile başlama,
+              <br />
+              2- Katardaki semboller yardımıyla diğer durumlara geçişler,
+              <br />
+              3- Bütün katar tükendiğinde kabul durumuna ulaşma ya da katarın
+              belirli bir konumunda reddetme şeklinde olmaktadır.
+            </p>
+            <p
+              id="formel-diller-tanimlar"
+              style={{ fontSize: '1.5em', marginBottom: '1em' }}
+            >
+              Formel Diller Tanımlar
+            </p>
+            <p className="notes-paragraph">
+              • ε veya λ boş katardır ve sembol içermez.
+            </p>
+            <p className="notes-paragraph">• (abba)**2 = abbaabba</p>
+            <p className="notes-paragraph">• (abba)**0 = λ</p>
+            <p className="notes-paragraph">
+              • Kleene Yıldızı (Kleene Star): ∑ üzerinde tanımlı olası bütün
+              katarlar kümesini gösterir.
+            </p>
+            <p className="notes-paragraph">
+              • Kleene Artısı (Kleene Plus): ∑ alfabesi için ∑+ bu alfabeden
+              oluşturulan boş katar hariç tüm katarların kümesini
+              göstermektedir.
+            </p>
+
+            <p className="notes-paragraph">
+              <strong></strong>
+            </p>
+            <p id="" style={{ fontSize: '1.5em', marginBottom: '1em' }}></p>
             <p className="notes-paragraph"></p>
           </div>
         </section>
